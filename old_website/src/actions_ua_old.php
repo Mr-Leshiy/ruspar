5c833601e07b8b42ef2b1c5dc152f276c8a52072
Pozhyla <mrbaymiks@gmail.com>
2025-09-14 11:51:45 +0300

    Equalised actions "ua" and "ru" parts

---
+++ old_website/src/actions_ua_old.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<? include ("meta_ua.inc"); ?>
+<title><? echo $TitleGlobal_Ua; ?> Акції</title>
+<script type="text/javascript" src="highslide/highslide.js"></script>
+<link rel="stylesheet" type="text/css" href="highslide/highslide.css">
+
+<script type="text/javascript">
+    hs.graphicsDir = 'highslide/graphics/';
+    hs.outlineType = 'rounded-white';
+</script>
+</head>
+
+<body bgcolor="#E8AC75" text="#000000" link="#000000" vlink="#000000" alink="#FF0000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" onLoad="MM_preloadImages('images/about_over.gif','images/service_over.gif','images/gallery_over.gif','images/contacts_over.gif')">
+<? include ("top_table.inc"); ?>
+<table width="725" border="0" cellspacing="0" cellpadding="0" align="center" background="images/bg_tab.gif">
+  <tr align="center" valign="top"> 
+    <td>
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr align="center" valign="top"> 
+          <td><img src="images/logo_down.gif" width="725" height="70"></td>
+        </tr>
+      </table>
+      
+<? include ("menu_table_ua.inc"); ?>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr valign="top">
+      <td>
+      <center><h1>Наші акції</h1></center>
+      <? include ("public_ua.inc"); ?> 
+
+          <br>
+          <br>
+
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+<? include ("bottom_table_ua.inc"); ?>
+</body>
+</html>
